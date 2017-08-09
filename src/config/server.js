612dfcd2291295da07d8c7968d6767a91f9df093
mcolburn <mac002@theColburns.us>
2017-08-09 08:44:46 -0400

    1.6.8 - removed hard coded server domains

--- src/config/server.js
+++ src/config/server.js
@@ -6,35 +6,15 @@ import axios from 'axios';
 const adminApi = "/admin/api/v1/";
 const dbApi = "/db/api/v1/";
 const user = "/user/";
-const useRemoteNet = false;
-const useRemoteOrg = false;
+//const useRemoteNet = false;
+//const useRemoteOrg = false;
 
 var getDbServer = () => {
-  if (document.location.hostname === "localhost" || document.location.hostname.endsWith("c9users.io")) {
-    if (useRemoteNet) {
-      return "https://www.ioc-liturgical-db.net";
-    } else if (useRemoteOrg) {
-      return "https://www.ioc-liturgical-db.org";
-    } else {
-      return document.location.protocol +  "//" + document.location.hostname + ":7474";
-    }
-  } else {
-    return document.location.protocol +  "//www." + document.location.hostname;
-  }
+  return document.location.protocol +  "//" + document.location.hostname + ":7474";
 }
 
 var getWsServer = () => {
-  if (document.location.hostname === "localhost") {
-    return document.location.protocol +  "//" + document.location.hostname + ":4567";
-  } else {
-    if (document.location.hostname.endsWith("net")) {
-      return document.location.protocol +  "//ioc-liturgical-db.net";
-//      return document.location.protocol +  "//ioc-liturgical-ws.net";
-    } else {
-      return document.location.protocol +  "//ioc-liturgical-db.org";
-//      return document.location.protocol +  "//ioc-liturgical-ws.org";
-    }
-  }
+  return document.location.protocol +  "//" + document.location.hostname + ":4567";
 }
 
 export default {
